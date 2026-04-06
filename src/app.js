a74e471937b5cdbaf526b93f36e64484deee21a4
Bhargav Reddy Allu <bhargav.allu@gmail.com>
2026-04-05 23:31:28 -0500

    Multiple Route Handlers

--- src/app.js
+++ src/app.js
@@ -3,24 +3,37 @@ const express = require('express');
 const app = express();
 
 
-//Dynamic Route
-app.get('/user/:userId/:name/:password', (req,resp)=>{
-    console.log(req.params);
-    resp.send({firstName:"Bhargav", lastName:"Allu"})
-})
+// //Dynamic Route
+// app.get('/user/:userId/:name/:password', (req,resp)=>{
+//     console.log(req.params);
+//     resp.send({firstName:"Bhargav", lastName:"Allu"})
+// })
 
-app.get('/user', (req,resp)=>{
-    resp.send({firstName:"Bhargav", lastName:"Allu"})
-})
+// app.get('/user', (req,resp)=>{
+//     resp.send({firstName:"Bhargav", lastName:"Allu"})
+// })
 
-app.post('/user', (req,resp)=>{ 
-    resp.send("Data Has been Posted");
-})
+// app.post('/user', (req,resp)=>{ 
+//     resp.send("Data Has been Posted");
+// })
 
-//.use will match all the http request methods.
-app.use('/test',(req, resp)=>{
-  resp.send("Hello From Test");  
-})
+// //.use will match all the http request methods.
+
+
+// a route can have multiple route handlers like below syntax
+app.use('/test',(req, resp, next)=>{
+    console.log("Resp handler 1")
+    next();
+    resp.send("Hello From Test"); 
+},
+(req,resp)=>{
+    console.log("Resp Handler2")
+    resp.send("sending from handler 2");
+}
+
+)
+// a route can have multiple route handlers like below syntax
+//app.use('route', ()=>{} //this is called route handler, ()=>{})
 
 app.listen(3000, ()=>{
     console.log("Server is Successfully listening on 3000:...")
