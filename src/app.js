00bf2ed6c9ff06666d53dd064596a41323dce00f
Bhargav Reddy Allu <bhargav.allu@gmail.com>
2026-04-06 02:56:44 -0500

    Delete and Update

--- src/app.js
+++ src/app.js
@@ -10,16 +10,14 @@ app.use(express.json());
 
 //Feed of Users
 app.get('/feedAllUsers', async (req,resp)=>{
-
-
     try{
         const users = await User.find({});
         resp.send(users); 
     }catch(err){
         resp.status(400).send("Something went Wrong")
     }
-
 })
+
 app.post('/signup', async (req, resp)=>{
     //Creating a new Instance of User Model
     const user = new User(req.body);
@@ -28,6 +26,31 @@ app.post('/signup', async (req, resp)=>{
         resp.send("User Successfully Added to Database");
     }catch(err){
         resp.status(400).send("Error Saving the user" + err.message);
+    }
+})
+
+//Delete User By Id
+
+app.delete('/deleteUser', async (req, resp)=>{
+    const userId = req.body.userId;
+    try {
+        const rep = await User.findByIdAndDelete(userId);
+        resp.send("User Deleted Succesfully");
+    }catch(err){
+        resp.status(400).send("Error Deleting the user" + err.message);
+    }
+})
+
+//update User by Patch
+app.patch("/updateUser", async (req,resp)=>{
+    
+    const userId = req.body.userId;
+    const body = req.body
+    try{
+        await User.findByIdAndUpdate({_id:userId},body )
+        resp.send("User Updated Successfully");
+    }catch(err){
+        resp.status(400).send("Error Updating the user" + err.message);
     }
 })
 
