7b6b72364c05f0a4c374d66848a89523f9d74653
Bhargav Reddy Allu <bhargav.allu@gmail.com>
2026-04-05 22:52:40 -0500

    Explore Get, Post API CALLS

--- src/app.js
+++ src/app.js
@@ -3,15 +3,21 @@ const express = require('express');
 const app = express();
 
 
-app.use('/',(req, resp)=>{
-  resp.send("Hello From Root");  
+
+
+app.get('/user', (req,resp)=>{
+    resp.send({firstName:"Bhargav", lastName:"Allu"})
 })
+
+app.post('/user', (req,resp)=>{ 
+    resp.send("Data Has been Posted");
+})
+
+//.use will match all the http request methods.
 app.use('/test',(req, resp)=>{
   resp.send("Hello From Test");  
 })
-app.use((req, resp)=>{
-  resp.send("Hello From Server");  
-})
+
 app.listen(3000, ()=>{
     console.log("Server is Successfully listening on 3000:...")
 });
